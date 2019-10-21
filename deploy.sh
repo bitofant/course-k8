541710763825f63c17aff5c4dacd0d24cc5b8e39
Jöran Tesse <joerantesse@gmail.com>
2019-10-21 10:56:06 -0400

    travis fix

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# echo Building images
-# docker build -t bitofant/multi-client:latest -t bitofant/multi-client:$SHA -f ./client/Dockerfile ./client
-# docker build -t bitofant/multi-server:latest -t bitofant/multi-server:$SHA -f ./server/Dockerfile ./server
-# docker build -t bitofant/multi-worker:latest -t bitofant/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+echo Building images
+docker build -t bitofant/multi-client:latest -t bitofant/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t bitofant/multi-server:latest -t bitofant/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t bitofant/multi-worker:latest -t bitofant/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-# echo Pushing images to hub.docker.com
-# docker push bitofant/multi-client:latest
-# docker push bitofant/multi-server:latest
-# docker push bitofant/multi-worker:latest
-# docker push bitofant/multi-client:$SHA
-# docker push bitofant/multi-server:$SHA
-# docker push bitofant/multi-worker:$SHA
+echo Pushing images to hub.docker.com
+docker push bitofant/multi-client:latest
+docker push bitofant/multi-server:latest
+docker push bitofant/multi-worker:latest
+docker push bitofant/multi-client:$SHA
+docker push bitofant/multi-server:$SHA
+docker push bitofant/multi-worker:$SHA
 
 echo Applying kubernetes changes
 kubectl apply -f k8s
