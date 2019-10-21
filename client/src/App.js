8904e6a7e8e73721cb7e9506d4fff8cda4976c3d
Jöran Tesse <joerantesse@gmail.com>
2019-10-21 15:44:41 -0400

    space added

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator version Kube</h1>
             <Link to="/">Home</Link>
-            &nbsp;&middot;&middot;
+            &nbsp;&middot;&nbsp;
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
